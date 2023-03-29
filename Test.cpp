6d0fa1fa14d897c462796b76440667e4241e6f88
Tamar Revazishvili <tamar.d154@gmail.com>
2023-03-29 13:24:58 +0300

    14 tests

--- Test.cpp
+++ Test.cpp
@@ -3,18 +3,55 @@
 #include "sources/game.hpp"
 #include "sources/card.hpp"
 
-TEST_CASE("Checking Initialization")
+TEST_CASE("Testing exceptions before initialization")
+{
+    Player p1("Alice");
+    Player p2("Bob");
+    CHECK_THROWS(p1.stacksize());
+    CHECK_THROWS(p2.cardesTaken());
+}
+
+TEST_CASE("Testing initialization")
 {
     Player p1("Alice");
     Player p2("Bob");
     Game game(p1, p2);
-    
+
+    CHECK(p1.cardesTaken() == 0);
+    CHECK(p1.stacksize() == 26);
+    CHECK(p2.cardesTaken() == 0);
+    CHECK(p2.stacksize() == 26);
 }
 
-TEST_CASE("Testing Game Class")
+TEST_CASE("Testing throws before playing any turn")
 {
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
+
+    CHECK_THROWS(game.printLastTurn());
+    CHECK_THROWS(game.printWiner());
 }
 
-TEST_CASE("Testing Card Class")
+TEST_CASE("Testing playing turns")
 {
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
+
+    CHECK_NOTHROW(game.playTurn());
+    CHECK((p1.stacksize() == 25 || p2.stacksize() == 25) == true);
+    CHECK_NOTHROW(game.playAll());
+    CHECK((p1.stacksize() == 0 && p2.stacksize() == 0) == true);
+}
+
+TEST_CASE("Testing throws after we get a winner")
+{
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1, p2);
+
+    game.playAll();
+    CHECK_THROWS(game.playTurn());
+    CHECK_THROWS(game.playAll());
 }
